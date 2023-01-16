db77840cb0d012c013eaa737ac5a1112a2375c55
Eric Fung <fungkinghong@gmail.com>
2023-01-17 01:05:31 +0800

    refactoring

--- Dental Points/draw_geometry.py
+++ Dental Points/draw_geometry.py
@@ -5,19 +5,9 @@ from variables import PIXEL_FOR_22MM
 
 
 def plot(data_dict, picture_location):
-    # plot seven points
-    x_coordinates = []
-    y_coordinates = []
-    for key in data_dict.keys():
-        if 'x' in key:
-            x_coordinates.append(data_dict[key])
+    point_seven_points(data_dict)
+    mid_6x, mid_6y, mid_5x, mid_5y, mid_4x, mid_4y = find_and_plot_mid_points(data_dict)
 
-        elif 'y' in key:
-            y_coordinates.append(data_dict[key])
-    plt.scatter(x_coordinates, y_coordinates, color='black', s=7, marker='v')
-
-    mid_6x, mid_6y, mid_5x, mid_5y, mid_4x, mid_4y = find_mid_points(data_dict)
-    plt.scatter([mid_6x, mid_5x, mid_4x], [mid_6y, mid_5y, mid_4y], color='red', s=25, marker='v')
 
     # plotting points 22mm away from 6M along line6
     plt.scatter([mid_6x + PIXEL_FOR_22MM * cos_slope6, mid_6x - PIXEL_FOR_22MM * cos_slope6],
@@ -44,13 +34,26 @@ def plot(data_dict, picture_location):
     plt.show()
 
 
+def point_seven_points(data_dict):
+    x_coordinates = []
+    y_coordinates = []
+    for key in data_dict.keys():
+        if 'x' in key:
+            x_coordinates.append(data_dict[key])
+
+        elif 'y' in key:
+            y_coordinates.append(data_dict[key])
+    plt.scatter(x_coordinates, y_coordinates, color='black', s=7, marker='v')
+
+
 def plot_pic(picture_location):
     img_test = cv2.imread(picture_location)
     plt.imshow(img_test)
 
 
-def find_mid_points(data_dict):
+def find_and_plot_mid_points(data_dict):
     mid_6x, mid_6y = get_mid_point(data_dict['6L_x'], data_dict['6L_y'], data_dict['6R_x'], data_dict['6R_y'])
     mid_5x, mid_5y = get_mid_point(data_dict['5L_x'], data_dict['5L_y'], data_dict['5R_x'], data_dict['5R_y'])
     mid_4x, mid_4y = get_mid_point(data_dict['4L_x'], data_dict['4L_y'], data_dict['4R_x'], data_dict['4R_y'])
+    plt.scatter([mid_6x, mid_5x, mid_4x], [mid_6y, mid_5y, mid_4y], color='red', s=25, marker='v')
     return mid_6x, mid_6y, mid_5x, mid_5y, mid_4x, mid_4y
