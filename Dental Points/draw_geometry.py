3bb6db7eed166f9a3094c4748cf21c9d214cec0f
Priyansh9900 <91251624+Priyansh9900@users.noreply.github.com>
2023-01-20 17:37:16 +0530

    Update draw_geometry.py

--- Dental Points/draw_geometry.py
+++ Dental Points/draw_geometry.py
@@ -1,59 +1,58 @@
 import cv2
 import matplotlib.pyplot as plt
-from required_functions import get_mid_point, plot_ellipse
-from variables import PIXEL_FOR_22MM
+from required_functions import *
+from variables import *
 
+def plot(data_dict,picture_location):
 
-def plot(data_dict, picture_location):
-    point_seven_points(data_dict)
-    mid_6x, mid_6y, mid_5x, mid_5y, mid_4x, mid_4y = find_and_plot_mid_points(data_dict)
+    def plot_lines(data_dict):
+        plt.plot([data_dict['6L_x'], data_dict['6R_x']], [data_dict['6L_y'], data_dict['6R_y']], color='blue', linewidth=1) #line6
+        plt.plot(x_perpendicular, y_perpendicular, color='green', linewidth=1) #line through mid6 peripendicular to line6
+        plt.plot(x_range, y_parallel, color='purple', linewidth=1) # line through C' parallel to line6
+        plt.plot(x_range, line_5, color='orange', linewidth=1) # line5
+        plt.plot(x_range, line_4, color='yellow', linewidth=1) # line4
+        # plt.plot(x_range, lin5_par6, color='purple', linewidth=1) # line through mid5 parallel to 6
+        # plt.plot(x_range, line4_par6, color='purple', linewidth=1) # line through mid4 parallel to 6
+        # plot_pic(IMAGE_LOCATION)
+    plot_lines(data_dict)
 
+    def plot_calculated_points(data_dict):
 
-    # plotting points 22mm away from 6M along line6
-    plt.scatter([mid_6x + PIXEL_FOR_22MM * cos_slope6, mid_6x - PIXEL_FOR_22MM * cos_slope6],
-                [mid_6y + PIXEL_FOR_22MM * sin_slope6, mid_6y - PIXEL_FOR_22MM * sin_slope6],
-                color='green', s=15, marker='v')
-    # plotting C' and C"
-    plt.scatter([c_x, c__x], [c_y, c__y], color='red', s=25, marker='+')
-    # plotting intersection points of ellipse and line 4,5
-    plt.scatter([coord_4[0], coord_4[1], coord_5[0], coord_5[1]], [coord_4[2], coord_4[3], coord_5[2], coord_5[3]],
-                color='blue', s=50, marker='+')
+        '''plotting mid points'''
+        plt.scatter([mid_6x, mid_5x, mid_4x], [mid_6y, mid_5y, mid_4y], color='red', s=25, marker='v')
 
-    plt.plot(x_per, y_per, color='green', linewidth=1)
-    plt.plot([data_dict['6L_x'], data_dict['6R_x']], [data_dict['6L_y'], data_dict['6R_y']], color='blue', linewidth=1)
-    plt.plot(x_par, y_par, color='purple', linewidth=1)
-    plt.plot(x_5, y_5, color='orange', linewidth=1)
-    plt.plot(x_4, y_4, color='yellow', linewidth=1)
-    plt.plot(x_5, y_5_par, color='purple', linewidth=1)
-    plt.plot(x_4, y_4_par, color='purple', linewidth=1)
+        '''plotting C' and C" '''
+        plt.scatter([c_x, c__x], [c_y, c__y], color='red', s=25, marker='+')
 
-    ax = plt.gca()
-    plot_ellipse(ax, center=(mid_6x, mid_6y), width=PIXEL_FOR_22MM, height=M6_C__, phi=ellipse_angle, color='red', lw=1)
+        '''plotting points 22mm away from mid6 along line 6'''
+        plt.scatter([mid_6x + PIXEL_FOR_22MM * cos_slope6, mid_6x - PIXEL_FOR_22MM * cos_slope6],
+                    [mid_6y + PIXEL_FOR_22MM * sin_slope6, mid_6y - PIXEL_FOR_22MM * sin_slope6],
+                    color='green', s=15, marker='v')
 
+        '''plotting intersection points of ellipse and line 4,5'''
+        plt.scatter([coord_4[0], coord_4[1], coord_5[0], coord_5[1]], [coord_4[2], coord_4[3], coord_5[2], coord_5[3]],
+                    color='blue', s=50, marker='+')
+    plot_calculated_points(data_dict)
+
+    def ellipse(data_dict):
+        ax = plt.gca()
+        plot_ellipse(ax, center=(mid_6x, mid_6y), width=PIXEL_FOR_22MM, height=M6_C__, phi=ellipse_angle, color='red', lw=1)
+    ellipse(data_dict)
+
+    def plot_seven_points(data_dict):
+        x_coordinates = []
+        y_coordinates = []
+        for key in data_dict.keys():
+            if 'x' in key:
+                x_coordinates.append(data_dict[key])
+
+            elif 'y' in key:
+                y_coordinates.append(data_dict[key])
+        plt.scatter(x_coordinates, y_coordinates, color='black', s=7, marker='v')
+    plot_seven_points(data_dict)
+
+    def plot_pic(picture_location):
+        img_test = cv2.imread(picture_location)
+        plt.imshow(img_test)
+        plt.show()
     plot_pic(picture_location)
-    plt.show()
-
-
-def point_seven_points(data_dict):
-    x_coordinates = []
-    y_coordinates = []
-    for key in data_dict.keys():
-        if 'x' in key:
-            x_coordinates.append(data_dict[key])
-
-        elif 'y' in key:
-            y_coordinates.append(data_dict[key])
-    plt.scatter(x_coordinates, y_coordinates, color='black', s=7, marker='v')
-
-
-def plot_pic(picture_location):
-    img_test = cv2.imread(picture_location)
-    plt.imshow(img_test)
-
-
-def find_and_plot_mid_points(data_dict):
-    mid_6x, mid_6y = get_mid_point(data_dict['6L_x'], data_dict['6L_y'], data_dict['6R_x'], data_dict['6R_y'])
-    mid_5x, mid_5y = get_mid_point(data_dict['5L_x'], data_dict['5L_y'], data_dict['5R_x'], data_dict['5R_y'])
-    mid_4x, mid_4y = get_mid_point(data_dict['4L_x'], data_dict['4L_y'], data_dict['4R_x'], data_dict['4R_y'])
-    plt.scatter([mid_6x, mid_5x, mid_4x], [mid_6y, mid_5y, mid_4y], color='red', s=25, marker='v')
-    return mid_6x, mid_6y, mid_5x, mid_5y, mid_4x, mid_4y
